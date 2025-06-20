ec8853ee2eb2d8dfce169afe7ff00ab7bd1d831b
isuriwebsrus <isurika@websrus.au>
2025-06-20 23:50:12 +0530

    Update config.php

--- db/config.php
+++ db/config.php
@@ -7,6 +7,7 @@ $dbname = "taskbuddy_db";
 $conn = new mysqli($host, $user, $password, $dbname);
 
 // Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
